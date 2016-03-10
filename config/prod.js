1ea0a3330e38718882c0e9f55cfc63b74864c9d9
James Allington-Kay <lancaster.1@hotmail.co.uk>
2016-03-10 20:07:55 +0000

    My Client Details

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 var config = {
   "url": "https://production-api.gmon.io",
-  "client_id": "",
-  "client_secret": ""
+  "client_id": "oauthclient_0000961vfkcdYGNP0ultI1",
+  "client_secret": "sdoa6dEPDCWJxqYNMBnL3Tu63gapwjZEahr9FLytFEHRBHPP3OiZ6fa/6SZppGYkftqSrPPHqfhG6kPzAZQO"
 };
